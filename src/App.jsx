1730f3e84056d880429278e96b6375e23f734a22
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-04-15 05:33:36 +0100

    Gsap-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,109 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useLayoutEffect, useRef } from "react";
 import "./App.css";
 import logo from "./img/Logo-white.png";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-  const ref1 = useRef(null);
+  const containerRef = useRef(null);
+  const headerRef = useRef(null);
+  const divRef = useRef(null);
 
-  // activating the Intersection Observer on the elements
-  useEffect(() => {
-    const observer1 = new IntersectionObserver(
-      ([entry]) => {
-        setIsIntersecting(entry.isIntersecting);
-      },
-      { rootMargin: "-300px" }
-    );
-    observer1.observe(ref1.current);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    let ctx = gsap.context(() => {
+      const tl = gsap
+        .timeline()
+        .from("#header", {
+          opacity: 0,
+          x: 100,
+          duration: 1,
+          ease: "ease-in",
+        })
+        .to("#header", {
+          x: 0,
+          opacity: 1,
+          duration: 0.5,
+          ease: "ease-out",
+        });
 
-    return () => {
-      observer1.disconnect();
-    };
+      const scrollAnimation = gsap
+        .timeline()
+        .fromTo(
+          ".child",
+          {
+            x: 0,
+            y: 0,
+            opacity: 0,
+            // duration:.01,
+          },
+          {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce",
+            scrollTrigger: {
+              trigger: ".child",
+              markers: true,
+              start: "top center",
+              end: "bottom bottom",
+              scrub: true,
+            },
+          }
+        )
+        .fromTo(
+          ".child-1",
+          {
+            x: 0,
+            y: 0,
+            opacity: 0,
+            // duration:.01,
+          },
+          {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce",
+            scrollTrigger: {
+              trigger: ".child-1",
+              markers: true,
+              start: "top center",
+              end: "bottom bottom",
+              scrub: true,
+            },
+          }
+        )
+        .fromTo(
+          ".child-2",
+          {
+            x: 0,
+            y: 0,
+            opacity: 0,
+            // duration:.01,
+          },
+          {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce",
+            scrollTrigger: {
+              trigger: ".child-2",
+              markers: true,
+              start: "top center",
+              end: "bottom bottom",
+              scrub: true,
+            },
+          }
+        );
+    });
+
+    return () => ctx.revert();
   }, []);
-
-  // when the targeted element is intersected
-  useEffect(() => {
-    if (isIntersecting) {
-      ref1.current.querySelectorAll("div").forEach((el) => {
-        el.classList.add("show");
-      });
-    } else {
-      ref1.current.querySelectorAll("div").forEach((el) => {
-        el.classList.remove("slide-in");
-      });
-    }
-  }, [isIntersecting]);
 
   return (
     <div className="App">
@@ -42,13 +113,25 @@ function App() {
         </span>
         <span className="large-txt">
           <h5>We've Gone</h5>
-          <h1>BETA!</h1>
+          <h1 id="header" ref={headerRef}>
+            BETA!
+          </h1>
         </span>
       </header>
-      <main ref={ref1}>
-        <div className="child-one">Customers</div>
-        <div className="child-two">Food Business</div>
-      </main>
+      <section className="grid">
+        <main ref={divRef}>
+          <div className="child">Customers</div>
+          <div className="child">Food Business</div>
+        </main>
+        <main ref={divRef}>
+          <div className="child-1">Customers</div>
+          <div className="child-1">Food Business</div>
+        </main>
+        <main ref={divRef}>
+          <div className="child-2">Customers</div>
+          <div className="child-2">Food Business</div>
+        </main>
+      </section>
       <footer>Eathub 2023</footer>
     </div>
   );
